f9f5ecdcd408592de04cba4cc4ba777c07e351d3
Faithfulness Alamu <vaguemail369@gmail.com>
2020-11-10 14:35:31 +0100

    Style todo item

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import './TodoItem.css'
 
 export default function TodoItem(props) {
 	return (
-		<div>
-			<input type="checkbox" checked={props.model.done}/>
-			<p>{props.model.content}</p>
+		<div id="todoitem">
+			<input id="checker" type="checkbox" checked={props.model.done}/>
+			<p id="content">{props.model.content}</p>
+			<div id="clearer" />
 		</div>
 	)
 }
