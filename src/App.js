4c950efc7be1a10dff39f8e95ba2cf2f25d6a0a9
Faithfulness Alamu <vaguemail369@gmail.com>
2020-11-10 14:07:58 +0100

    Build entry component
      - Allow user enter a new todo item
      - Save todo item in app state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 import Entry from './Entry.js';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-	  <h1>Todo App</h1>
-	  <Entry />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+	  super(props)
+	
+	  this.state = {
+		  todos: []
+	  }
+  }
+
+  addEntry = (todoItem) => {
+	console.log(todoItem)
+
+	this.setState(state => (
+		{todos: state.todos.concat(todoItem)}
+	))
+  }
+
+  render() {
+	  return (
+		<div className="App">
+		  <h1>Todo App</h1>
+		  <Entry onAddEntry={this.addEntry} />
+		</div>
+	  );
+  }
 }
 
 export default App;
--- src/Entry.js
+++ src/Entry.js
@@ -1,8 +1,35 @@
-export default function Entry(props) {
-	return (
-		<div>
-			<input type="text"/>
-			<button>Add</button>
-		</div>
-	)
+import React from 'react'
+
+export default class Entry extends React.Component {
+	constructor(props) {
+		super(props)
+	
+		this.state = {
+			entryText: ""
+		}
+	}
+
+	updateState = (e) => {
+		this.setState({
+			entryText: e.target.value
+		})
+	}
+
+	addEntry = () => {
+		let todoItem = {
+			content: this.state.entryText,
+			done: false
+		}
+
+		this.props.onAddEntry(todoItem)
+	}
+
+	render() {
+		return (
+			<div>
+				<input onChange={this.updateState} value={this.state.entryText} type="text"/>
+				<button onClick={this.addEntry}>Add</button>
+			</div>
+		)
+	}
 }
