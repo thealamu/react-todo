eae49b0f9fb2aa7d8c14e348682c0fdb5b6e952e
Faithfulness Alamu <vaguemail369@gmail.com>
2020-11-10 14:46:49 +0100

    Render pending todo items

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,9 +2,20 @@ import React from 'react'
 import TodoItem from './TodoItem.js'
 
 export default function TodoList(props) {
+	let undone = props.todos.filter(item => !item.done)
+	let items = []
+
+	undone.forEach(item => {
+		items.push(
+			<>
+				<TodoItem model={item} />			
+			</>
+		)}
+	)
+
 	return (
 		<>
-			<TodoItem model={{content: "do stuff", done: true}} />			
+			{items}
 		</>
 	)
 }
